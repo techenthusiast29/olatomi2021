37b213a04829d36549a68b56e005f8d21e4d27b4
techenthusiast29 <59942767+techenthusiast29@users.noreply.github.com>
2021-09-12 00:44:47 +0100

    Update biola.js
    Js time

--- biola.js
+++ biola.js
@@ -1,4 +1,4 @@
- var countDownDate = new Date("Dec 11, 2021 10:00:00").getTime();
+ var countDownDate = new Date("Dec 11, 2021 8:00:00").getTime();
 
  // Update the count down every 1 second
  var x = setInterval(function() {
