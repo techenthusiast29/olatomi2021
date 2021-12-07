9350f76f7d96c750c2ada7a82c17aacc64a052e3
techenthusiast29 <59942767+techenthusiast29@users.noreply.github.com>
2021-12-07 07:27:23 +0100

    Add files via upload

--- biola.js
+++ biola.js
@@ -22,6 +22,6 @@
      // If the count down is finished, write some text
      if (distance < 0) {
          clearInterval(x);
-         document.getElementById("time").innerHTML = "EXPIRED";
+         document.getElementById("time").innerHTML = "Happy Married Life";
      }
  }, 1000);
